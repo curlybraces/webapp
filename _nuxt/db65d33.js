a9fbe89f1ba5be167d88b3619abe16ea80b76401
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-23 17:27:31 +0000

    Deploy ospic/webapp to github.com/ospic/webapp.git:gh-pages

---
+++ _nuxt/db65d33.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{520:function(n,o,l){"use strict";l.r(o);var e=l(59),component=Object(e.a)({},void 0,void 0,!1,null,null,null);o.default=component.exports}}]);
